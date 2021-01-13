463a19cea4f63b21a2229950900a60acb08a1335
Dani Coleman <danicoleman00@gmail.com>
2021-01-13 14:13:31 -0700

    Add the destination-weather/id route to the destination-weather show action

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :destinations
+      get '/destination-weather/:id', to: "destination_weather#show"
       # only: [:index, :show, :create, :update, :destroy]
     end
   end
