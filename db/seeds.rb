fb7c28913ec4da4ca008f1c4e6de560595cc5157
Dani Coleman <danicoleman00@gmail.com>
2021-01-14 09:00:25 -0700

    Update seeds file to have real zip codes

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,16 +6,56 @@ class Seed
   end
 
   def generate_destinations
-    50.times do |i|
-      dest = Destination.create!(
-        name: Faker::Address.city,
-        zip: Faker::Address.zip,
-        description: Faker::Lorem.sentence,
-        image_url: Faker::Placeholdit.image
-      )
-      puts "Destination #{i}: #{dest.name} created!"
+    dest1 = Destination.create!(
+      name: Faker::Address.city,
+      zip: 37650,
+      description: Faker::Lorem.sentence,
+      image_url: Faker::Placeholdit.image
+    )
+    dest2 = Destination.create!(
+      name: Faker::Address.city,
+      zip: 80241,
+      description: Faker::Lorem.sentence,
+      image_url: Faker::Placeholdit.image
+    )
+    dest3 = Destination.create!(
+      name: Faker::Address.city,
+      zip: 29210,
+      description: Faker::Lorem.sentence,
+      image_url: Faker::Placeholdit.image
+    )
+    dest3 = Destination.create!(
+      name: Faker::Address.city,
+      zip: 98362,
+      description: Faker::Lorem.sentence,
+      image_url: Faker::Placeholdit.image
+    )
+    dest3 = Destination.create!(
+      name: Faker::Address.city,
+      zip: 59802,
+      description: Faker::Lorem.sentence,
+      image_url: Faker::Placeholdit.image
+    )
+
+    puts "Destination #{i}: #{dest.name} created!"
+
+    # 5.times do |i|
+    #   dest = Destination.create!(
+    #     name: Faker::Address.city,
+    #     zip: 37650,
+    #     description: Faker::Lorem.sentence,
+    #     image_url: Faker::Placeholdit.image
+    #   )
+    #   puts "Destination #{i}: #{dest.name} created!"
+    # end
+    # 5.times do |i|
+    #   dest = Destination.create!(
+    #     name: Faker::Address.city,
+    #     zip: 80005,
+    #     description: Faker::Lorem.sentence,
+    #     image_url: Faker::Placeholdit.image
+    #   )
     end
   end
-end
 
 Seed.start
