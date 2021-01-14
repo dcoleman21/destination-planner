c6322c86ea625f397d2c47cf2fe8e8b3cc29930d
Dani Coleman <danicoleman00@gmail.com>
2021-01-14 07:52:07 -0700

    Add destination weather poro test making sure it returns data for weather, image and destination data based on location

--- spec/poros/destination_weather_spec.rb
+++ spec/poros/destination_weather_spec.rb
@@ -1,22 +1,27 @@
 require 'rails_helper'
 
 describe "Destination Weather" do
-  it "creates objects" do
-  data = {
-            "data": {
-              "id": "206",
-              "type": "destination-weather",
-              "attributes": {
-                "date": "Wednesday-January-13-2021",
-                "current_temp": 41.79,
-                "high_temp": 44.6,
-                "low_temp": 39,
-                "description": "clear sky",
-                "name": "Olympic National Park",
-                "image_url": "https://media3.giphy.com/media/l1J9IQ6b9EtsW9QFG/giphy.gif?cid=c0f5ef4dxjv2j9aphtei6nctj43f6gyd929b8h7nbgqve01b&rid=giphy.gif",
-                "zip": "98362"
-              }
-            }
-          }
+  it "exists" do
+    destination = Destination.create(
+      id: 1,
+      name: "Arvada",
+      zip: 80005,
+      description: "Growing fast",
+      image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8wEsMvmI4Q6CbEoakZMiyc8Kd437htv6e8Q&usqp=CAU"
+    )
+    weather = WeatherFacade.fetch_weather_data(80005)
+    image = ImageFacade.fetch_image_data(weather.description)
+
+    destination_weather = DestinationWeather.new(destination, weather, image)
+
+    expect(destination_weather.id).to eq(destination.id)
+    expect(destination_weather.date).to eq(weather.date)
+    expect(destination_weather.current_temp).to eq(weather.current_temp)
+    expect(destination_weather.high_temp).to eq(weather.high_temp)
+    expect(destination_weather.low_temp).to eq(weather.low_temp)
+    expect(destination_weather.description).to eq(weather.description)
+    expect(destination_weather.name).to eq(destination.name)
+    expect(destination_weather.image_url).to eq(image.image_url)
+    expect(destination_weather.zip).to eq(destination.zip)
   end
 end
